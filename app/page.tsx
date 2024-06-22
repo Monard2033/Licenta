d2ff95275649b5dfd18664c0348340e40408d3bb
Monard2033 <opinca2017@gmail.com>
2024-06-22 19:02:07 +0300

    31-th commit

--- app/page.tsx
+++ app/page.tsx
@@ -25,9 +25,9 @@ export default function Index() {
             const isAdmin = await checkAdminRole();
             // @ts-ignore
             setIsAdmin(isAdmin);
-            setLoading(false);
         };
-        fetchUser();
+        fetchUser()
+        setLoading(false);
     }, []);
 
 
