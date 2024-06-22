f032f8158d8963274d108e16af8ba23f65f97ac2
MIHAIL OPINCA <79156533+Monard2033@users.noreply.github.com>
2024-06-22 20:56:29 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -60,17 +60,17 @@ export default function Index() {
                     <div id="SessiuniTile" className="w-[25%] h-full shadow-xl bg-content1 border-3 rounded-medium flex flex-col">
                         <div className="h-[80%] rounded-medium">
                             <a href="/main/sessions" className="flex items-center justify-center h-full">
-                                <Image alt={"Sesiuni"} className="w-full h-full rounded-medium object-cover" priority={false} src={"/main/TileImage.png"} width={512} height={512}/>
+                                <Image alt={"Sesiuni"} className="w-full h-full rounded-medium object-cover" src={"/main/TileImage.png"} width={512} height={512}/>
                             </a>
                         </div>
-                        <div className="flex items-center justify-around h-[25%] border-2 rounded-medium mt-auto">
+                        <div className="flex items-center justify-around h-[25%] bg-content1 border-2 rounded-medium mt-auto">
                             <button className="text-blue-600 hover:text-blue-800" onClick={e => router.push("/sessions")}>Sesiuni</button>
                         </div>
                     </div>
                     <div id="ProiecteTile" className="w-[25%] h-full shadow-xl bg-content1 border-3 rounded-medium flex flex-col">
                         <div className="h-[80%] rounded-medium">
                             <a href="/main/projects" className="flex items-center justify-center h-full">
-                                <Image alt={"Proiecte"} className="w-full h-full rounded-medium object-cover" priority={false} src={"/main/TileImage2.png"} width={512} height={512}/>
+                                <Image alt={"Proiecte"} className="w-full h-full rounded-medium object-cover" src={"/main/TileImage2.png"} width={512} height={512}/>
                             </a>
                         </div>
                         <div className="flex items-center justify-around h-[25%] bg-content1 border-2 rounded-medium mt-auto">
